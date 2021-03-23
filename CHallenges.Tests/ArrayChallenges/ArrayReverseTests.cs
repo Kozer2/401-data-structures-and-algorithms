0918cc4e7ba2554b1a56aa71932d6099225d067b
Kozer2 <benhemann@yahoo.com>
2021-03-23 13:44:33 -0500

    testing new test system

---
+++ CHallenges.Tests/ArrayChallenges/ArrayReverseTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges.Tests.ArrayChallenges
+{
+    class ArrayReverseTests
+    {
+        [Fact]
+        public void Reverse_returns_empty_array_given_empty_array()
+        {   
+            // arrange
+            int[] array = new int[0];
+
+            // act
+            ArrayReverse.Reverse(array);
+
+
+            // assert
+            Assert.Empty(result);
+
+        }
+    }
+}
