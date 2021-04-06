7a0a307bc842b4977db815fff86575ebef99de9f
Kozer2 <benhemann@yahoo.com>
2021-04-06 14:18:44 -0500

    got enqueue passing

---
+++ CHallenges.Tests/AnimalShelterTests/AnimalShelterTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Challenges.AnimalShelter;
+
+namespace Challenges.Tests.AnimalShelterTests
+{
+   public class AnimalShelterTests
+    {
+        [Fact]
+        public void Enqueue_and_IsEmpty_work()
+        {
+            // arrange
+            AnimalShelter<string> dog = new AnimalShelter<string>();
+            // act
+            dog.Enqueue("Dog");
+
+
+            // assert
+            Assert.False(dog.IsEmpty);
+
+        }
+
+       
+
+
+
+
+
+    }
+
+
+    
+}
+
+
---
+++ Challenges/AnimalShelter/AnimalShelter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures.StackNQueue;
+
+namespace Challenges.AnimalShelter
+{
+    public class AnimalShelter<T> : IQueue<T>
+    {
+
+        private DataStructures.StackNQueue.Stack<T> Storage = new DataStructures.StackNQueue.Stack<T>();
+        private DataStructures.StackNQueue.Stack<T> ReverseStack = new DataStructures.StackNQueue.Stack<T>();
+        private DataStructures.StackNQueue.Stack<T> Holding = new DataStructures.StackNQueue.Stack<T>();
+
+ 
+
+        public bool IsEmpty => Storage.isEmpty() && ReverseStack.isEmpty();
+
+        public T Dequeue()
+        {
+          
+            return ReverseStack.Pop();
+        }
+
+        public void Enqueue(T value)
+        {
+       
+            Storage.Push(value);
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
