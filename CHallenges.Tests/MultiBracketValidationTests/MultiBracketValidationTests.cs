43af1cd7f375d345da8c4882f160a73761b8b555
Kozer2 <benhemann@yahoo.com>
2021-04-08 09:16:37 -0500

    got some tests running

--- CHallenges.Tests/MultiBracketValidationTests/MultiBracketValidationTests.cs
+++ CHallenges.Tests/MultiBracketValidationTests/MultiBracketValidationTests.cs
@@ -8,17 +8,49 @@ using Challenges.MultiBracketChallenge;
 
 namespace Challenges.Tests.MultiBracketValidationTests
 {
-   public class MultiBracketValidationTests
+    public class MultiBracketValidationTests
     {
+        [Fact]
+        public void MultiBracketValidation_returns_false_if_string_is_empty()
+        {
+            // arrange
+            string input = "";
+
+            // act
+            bool result = MultiBracketValidationClass.CheckBracketBalance(input);
+
+            // assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void MultiBracketValidation_returns_false_if_string_has_single_character()
+        {
+            // arrange
+            string input = "{";
+
+            // act
+            bool result = MultiBracketValidationClass.CheckBracketBalance(input);
+
+            // assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void MultiBracketValidation_returns_false_if_string_has_no_brackets()
+        {
+            // arrange
+            string input = "tests";
+
+            // act
+            bool result = MultiBracketValidationClass.CheckBracketBalance(input);
+
+            // assert
+            Assert.False(result);
+        }
         
 
-
-
-
     }
-
-
-    
 }
 
 
