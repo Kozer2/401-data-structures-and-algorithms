4cb1da8d4aba3e25b5fc5af1133b3a6fe993a87d
Kozer2 <benhemann@yahoo.com>
2021-04-26 14:32:01 -0500

    added 2 more working tests and insertionSort code

--- CHallenges.Tests/ArrayChallenges/InsertionSortTests.cs
+++ CHallenges.Tests/ArrayChallenges/InsertionSortTests.cs
@@ -11,7 +11,7 @@ namespace Challenges.Tests.ArrayChallenges
    public class InsertionSortTests
     {
         [Fact]
-        public void InsertionSort_works()
+        public void InsertionSort_returns_an_array()
         {
             // arrange
             int[] arr = new int[6] { 8, 4, 23, 42, 16, 15 };
@@ -24,5 +24,39 @@ namespace Challenges.Tests.ArrayChallenges
             //assert
             Assert.Equal(arr, result);
         }
+
+
+        [Fact]
+        public void InsertionSort_returns_a_sorted_array()
+        {
+            // arrange
+            int[] arr = new int[6] { 8, 4, 23, 42, 16, 15 };
+            int[] sortedArr = new int[6] { 4, 8, 15, 16, 23, 42 };
+
+
+            // act
+            int[] result = InsertionSort.InsertionSortFunc(arr);
+
+
+            //assert
+            Assert.Equal(sortedArr, result);
+        }
+
+        [Fact]
+        public void InsertionSort_works_with_array_with_multiple_same_numbers()
+        {
+            // arrange
+            int[] arr = new int[12] { 8, 4, 23, 42, 16, 15, 4, 8, 15, -1, 12, 100 };
+            int[] sortedArr = new int[12] { -1, 4, 4, 8, 8, 12, 15, 15, 16, 23, 42, 100 };
+
+
+            // act
+            int[] result = InsertionSort.InsertionSortFunc(arr);
+
+
+            //assert
+            Assert.Equal(sortedArr, result);
+        }
+
     }
 }
--- Challenges/ArrayChallenges/InsertionSort.cs
+++ Challenges/ArrayChallenges/InsertionSort.cs
@@ -10,6 +10,19 @@ namespace Challenges.ArrayChallenges
     {
         public static int[] InsertionSortFunc(int[] arr)
         {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int valueKey = arr[i];
+                int j = i - 1;
+
+                while(j >= 0 && arr[j] > valueKey)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                    arr[j + 1] = valueKey;
+                }
+
+            }
             return arr;
         }
     }
