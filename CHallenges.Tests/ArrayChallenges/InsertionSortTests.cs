b8fe94bf2f67c1462515076e4fea04649b8f9f3a
Kozer2 <benhemann@yahoo.com>
2021-04-26 14:23:45 -0500

    insertionSort returns an array

---
+++ CHallenges.Tests/ArrayChallenges/InsertionSortTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Challenges.ArrayChallenges;
+
+namespace Challenges.Tests.ArrayChallenges
+{
+   public class InsertionSortTests
+    {
+        [Fact]
+        public void InsertionSort_works()
+        {
+            // arrange
+            int[] arr = new int[6] { 8, 4, 23, 42, 16, 15 };
+
+
+            // act
+            int[] result = InsertionSort.InsertionSortFunc(arr);
+
+
+            //assert
+            Assert.Equal(arr, result);
+        }
+    }
+}
