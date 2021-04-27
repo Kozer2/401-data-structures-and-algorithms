6680c10da5580c46f935c0ff1e7f0ade3fd78362
Kozer2 <benhemann@yahoo.com>
2021-04-27 14:29:09 -0500

    first test dont. Merge sort returns an array

---
+++ CHallenges.Tests/ArrayChallenges/MergeSortTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Challenges.ArrayChallenges;
+
+namespace Challenges.Tests.ArrayChallenges
+{
+   public class MergeSortTests
+    {
+        [Fact]
+        public void MergeSort_returns_an_array()
+        {
+            // arrange
+            int[] arr = new int[6] { 8, 4, 23, 42, 16, 15 };
+
+
+            // act
+            int[] result = MergeSortChallenge.MergeSort(arr);
+
+
+            //assert
+            Assert.Equal(arr, result);
+        }
+
+
+        
+
+    }
+}
---
+++ Challenges/ArrayChallenges/MergeSortChallenge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges.ArrayChallenges
+{
+   public class MergeSortChallenge
+    {
+        public static int[] MergeSort(int[] arr)
+        {
+            return arr;        
+        }
+    }
+}
